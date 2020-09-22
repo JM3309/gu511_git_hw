5809e19a62e35731ac6acad48e79827b19dfb560
Iris <jm3309>
2020-09-22 15:20:51 +0000

    helloworld:initial commit

--- helloworld.py
+++ helloworld.py
@@ -1,1 +1,9 @@
-# 511 github repo
+import datetime
+ 
+def hello_world():
+    print('hello world!')
+    now = datetime.datetime.now()
+    print(f'it is {now}')
+ 
+if __name__ == __main__:
+    hello_world()
